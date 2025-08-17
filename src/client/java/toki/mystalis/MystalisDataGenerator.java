c99b13504be14ecf97dc39a98beabe07ac5f3933
toki <clevertrever31@gmail.com>
2025-08-17 16:31:54 -0500

    Initial Setup

---
+++ src/client/java/toki/mystalis/MystalisDataGenerator.java
@@ -0,0 +1,11 @@
+package toki.mystalis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MystalisDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/toki/mystalis/Mystalis.java
@@ -0,0 +1,16 @@
+package toki.mystalis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mystalis implements ModInitializer {
+	public static final String MOD_ID = "mystalis";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
